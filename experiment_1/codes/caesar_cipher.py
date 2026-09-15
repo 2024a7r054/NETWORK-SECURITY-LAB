e7131368452bc282a8ec0c7b314e96c3368caa35
2024a7r054 <2024a7r054@mietjammu.in>
2026-09-16 02:41:10 +0530

    Update caesar_cipher.py

--- experiment_1/codes/caesar_cipher.py
+++ experiment_1/codes/caesar_cipher.py
@@ -1,1 +1,22 @@
+def caesar_cipher(text: str, shift: int) -> str:
+    result = []
 
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                base = ord('A')
+                new_char = chr((ord(char) - base + shift) % 26 + base)
+                result.append(new_char)
+            elif char.islower():
+                base = ord('a')
+                new_char = chr((ord(char) - base + shift) % 26 + base)
+                result.append(new_char)
+        else:
+
+            result.append(char)
+
+    return "".join(result)
+text_input = input("Enter your message: ")
+shift_input = int(input("Enter shift number: "))
+encrypted_message = caesar_cipher(text_input, shift_input)
+print("Encrypted Message:", encrypted_message)
